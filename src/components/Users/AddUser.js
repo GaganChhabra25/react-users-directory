42a0ddd1f3c0828b212db5cb348809aa3da6024e
Gagan Chhabra <gagan.chhabra1990@gmail.com>
2021-04-27 17:08:44 +0530

    Use of refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,41 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import Card from '../UI/Card'
 import './AddUser.css'
 import ErrorModal from './../UI/ErrorModal';
 
  const AddUser = (props) => {
-    const [enteredUserName, setEnteredUserName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef =  useRef();
+    const ageInputRef =  useRef();
+
     const [error, setError] = useState();
 
      const handleAddUser = (event) => {
         event.preventDefault();
-        if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values)'
             });
             return;
         }
-        if(+enteredAge < 1) {
+        if(+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0)('
             });
             return;
         }
-        props.handleAddUser(enteredUserName, enteredAge);
-        setEnteredAge('');
-        setEnteredUserName('');
+        props.handleAddUser(enteredName, enteredUserAge);
+        // Don't use this
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
      }
-
-     const handleUsernameChange = (event) => {
-         setEnteredUserName(event.target.value);
-     }
-
-     const handleAgeChange = (event) => {
-        setEnteredAge(event.target.value);
-    }
 
     const errorHandler = () => {
         setError(null);
@@ -56,15 +53,13 @@ import ErrorModal from './../UI/ErrorModal';
                     <input 
                         type="text" 
                         id="username" 
-                        onChange={handleUsernameChange} 
-                        value={enteredUserName}
+                        ref={nameInputRef}
                     />
                     <label htmlFor="age">Age (Years)</label>
                     <input 
                         type="number"
                         id="age"
-                        onChange={handleAgeChange} 
-                        value={enteredAge}
+                        ref={ageInputRef}
                     />
                     <button type="submit">Add User</button>
                 </form>
